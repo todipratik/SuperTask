13b06be73497bda4e571b67ae41f1cea4e68d4ef
Pratik Todi <ptodi.y12@lnmiit.ac.in>
2015-10-20 13:49:16 +0530

    bookmarked images displayed in BookmarkedFragment

--- app/src/main/java/com/supertask/fragments/BookmarkedFragment.java
+++ app/src/main/java/com/supertask/fragments/BookmarkedFragment.java
@@ -5,26 +5,33 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import com.supertask.Bookmark;
 import com.supertask.BookmarkDbHelper;
+import com.supertask.DisplayImageAdapter;
 import com.supertask.R;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pratik on 18/10/15.
  */
 public class BookmarkedFragment extends Fragment {
 
-    TextView textView;
+    private TextView bookmarkTextView;
+    private GridView imageGridView;
+    private List<String> pathOfImageFiles;
+    private DisplayImageAdapter displayImageAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bookmarked, container, false);
-        textView = (TextView) view.findViewById(R.id.textView2);
-        textView.setText("hey this is your bookmarked tab!!!\n");
+        bookmarkTextView = (TextView) view.findViewById(R.id.bookmark_text_view);
+        imageGridView = (GridView) view.findViewById(R.id.bookmarked_grid_view);
+        bookmarkTextView.setText(R.string.no_bookmark_message);
         return view;
     }
 
@@ -32,11 +39,16 @@ public class BookmarkedFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         BookmarkDbHelper bookmarkDbHelper = new BookmarkDbHelper(getActivity());
-        ArrayList<Bookmark> a = bookmarkDbHelper.getAllBookmark();
-        textView.append("The size is: " + a.size() + "\n");
-        for (Bookmark b : a) {
-            textView.append(b.getShirt() + "\n");
-            textView.append(b.getPant() + "\n\n");
+        ArrayList<Bookmark> bookmarks = bookmarkDbHelper.getAllBookmark();
+        pathOfImageFiles = new ArrayList<>();
+        if (bookmarks.size() > 0) {
+            bookmarkTextView.setText("The number of bookmarked items found: " + bookmarks.size());
+            for (Bookmark bookmark : bookmarks) {
+                pathOfImageFiles.add(bookmark.getShirt());
+                pathOfImageFiles.add(bookmark.getPant());
+            }
+            displayImageAdapter = new DisplayImageAdapter(getActivity(), pathOfImageFiles);
+            imageGridView.setAdapter(displayImageAdapter);
         }
     }
 }
